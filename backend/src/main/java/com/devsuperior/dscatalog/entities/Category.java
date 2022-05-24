da216e49e76e747f612db1fa7f9946ff8830d318
Luis Ribeiro <luis.ribeiro@unicred.com.br>
2022-05-23 21:03:04 -0300

    Product entity, SQL script

--- backend/src/main/java/com/devsuperior/dscatalog/entities/Category.java
+++ backend/src/main/java/com/devsuperior/dscatalog/entities/Category.java
@@ -22,10 +22,10 @@ public class Category implements Serializable {
 	private Long id;
 	private String name;
 	
-	//@Column(columnDefinition = "TIMESTAMP WHITOUT TIME ZONE")
+	@Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
 	private Instant createdAt;
 	
-	//@Column(columnDefinition = "TIMESTAMP WHITOUT TIME ZONE")
+	@Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
 	private Instant updateAt;
 	
 	
