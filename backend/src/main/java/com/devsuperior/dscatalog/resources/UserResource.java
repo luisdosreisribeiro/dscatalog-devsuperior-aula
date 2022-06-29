4d422e7b210f13deae47a99900d6e53483d79274
Luis Ribeiro <luis.ribeiro@unicred.com.br>
2022-06-29 14:44:55 -0300

    Custom validation response. And validations basics users.

--- backend/src/main/java/com/devsuperior/dscatalog/resources/UserResource.java
+++ backend/src/main/java/com/devsuperior/dscatalog/resources/UserResource.java
@@ -2,6 +2,8 @@ package com.devsuperior.dscatalog.resources;
 
 import java.net.URI;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -42,7 +44,7 @@ public class UserResource {
 	}
 	
 	@PostMapping
-	public ResponseEntity<UserDTO>salvar(@RequestBody UserInsertDTO dto){
+	public ResponseEntity<UserDTO>salvar(@Valid @RequestBody UserInsertDTO dto){
 		UserDTO newDto = userService.salvar(dto);
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
 				.buildAndExpand(newDto.getId()).toUri();
@@ -54,7 +56,7 @@ public class UserResource {
 	
 	
 	@PutMapping(value = "/{id}")
-	public ResponseEntity<UserDTO>atualizar(@RequestBody UserDTO dto, @PathVariable Long id){
+	public ResponseEntity<UserDTO>atualizar(@Valid @RequestBody UserDTO dto, @PathVariable Long id){
 		
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
 				.buildAndExpand(dto.getId()).toUri();
